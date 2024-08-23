d7fff8c25283712d9a18fefcc7f8e8d6efaee1af
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-08-23 17:25:11 -0500

    chore: Add TopHeader component to Home page

---
+++ src/components/ui/topheader.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function TopHeader() {
+  return (
+    <header className="flex items-center justify-center p-4 w-[100%]">
+      <h1 className="text-2xl font-bold">Logo</h1>
+    </header>
+  );
+}
