f079de2d76013ed2a66c9791e41c97849c60a077
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-08-23 21:38:18 -0500

    chore: Add TopHeader component to Home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import TopHeader from "@/components/ui/topheader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TopHeader />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import TopHeader from "@/components/ui/topheader";
-
 export default function Home() {
   return (
     <>
-      <TopHeader />
       <main className="m-0 p-0">
         <h1 className="text-2xl font-bold">Home</h1>
       </main>
