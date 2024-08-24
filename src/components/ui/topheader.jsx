fb32a2c8e58c349f8b601961a726c7dacf71921d
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-08-23 21:38:34 -0500

    feat: Update TopHeader component with logo and mode toggle

--- src/components/ui/topheader.jsx
+++ src/components/ui/topheader.jsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
+import { ModeToggle } from "./mode-toggle";
 
 export default function TopHeader() {
   return (
-    <header className="flex items-center justify-center p-4 w-[100%]">
-      <h1 className="text-2xl font-bold">Logo</h1>
+    <header className="grid grid-cols-3 grid-rows-1 p-4 w-[100%]">
+      {/* <h1 className="text-2xl font-bold">Logo</h1> */}
+      <Link href="/" className="col-start-2 justify-self-center">
+        <Image src={`/daily.svg`} alt="Daily" width={128} height={128} />
+      </Link>
+      <section className="justify-self-end self-center p-4">
+        <ModeToggle />
+      </section>
     </header>
   );
 }
