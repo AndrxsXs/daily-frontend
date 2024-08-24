dcfc94645333452301e5fa587ff0a1d7b54b6a11
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-08-24 17:01:17 -0500

    feat: Add link and button to start page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
     <>
-      <main className="m-0 p-0 w-[100%] h-[60dvh]">
+      <main className="m-0 p-0 w-[100%] h-[100%]">
         <article className="items-center grid grid-cols-2 grid-rows-1 gap-10 p-4 mx-auto w-[70%] h-[100%]">
           <Image
             src={`/hero.jpeg`}
             alt="Hero Image"
             width={500}
             height={500}
-            className="rounded-lg justify-self-end"
+            className="rounded-lg justify-self-end shadow-lg"
           />
           <section>
             <header>
@@ -27,6 +30,11 @@ export default function Home() {
               Daily es una aplicación web que te permite organizar tu día a día
               de manera fácil y rápida.
             </p>
+            <Link href="/start">
+              <Button className="mt-4">
+                Comenzar <ArrowRight className="ml-2" />
+              </Button>
+            </Link>
           </section>
         </article>
       </main>
