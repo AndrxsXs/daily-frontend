7bd179d7a1719ea91dbe65ddc27c417f8a46b8f7
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-08-24 19:24:07 -0500

    hover para los títulos.

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -10,25 +10,39 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import Link from "next/link";
+import { CircleHelp } from "lucide-react";
+import { LayoutList } from "lucide-react";
+import { ListChecks } from "lucide-react";
+import { Plus } from "lucide-react";
 
 export default function Page() {
   return (
     <div className="grid justify-center">
       <Tabs defaultValue="account" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="earring">Pendientes</TabsTrigger>
-          <TabsTrigger value="finished">Finalizadas</TabsTrigger>
-          <TabsTrigger value="create">Crear Tarea</TabsTrigger>
+          <TabsTrigger value="earring">
+            <span className="px-2 py-1 rounded transition-all duration-300 text-[#683142] hover:bg-white hover:shadow-md">
+              <LayoutList className="inline h-4 w-4" /> Pendientes
+            </span>
+          </TabsTrigger>
+          <TabsTrigger value="finished">
+            <span className="px-2 py-1 rounded transition-all duration-300 text-[#683142] hover:bg-white hover:shadow-md">
+              <ListChecks className="inline h-4 w-4" /> Finalizadas
+            </span>
+          </TabsTrigger>
+          <TabsTrigger value="create">
+           <span className="px-2 py-1 rounded transition-all duration-300 text-[#683142] hover:bg-white hover:shadow-md">
+              <Plus className="inline h-4 w-4" /> Crear tarea
+            </span>
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="earring">
           <Card>
             <CardHeader>
               <CardDescription className="text-center">
-                ¡Hola care pipi! 
+                ¡Hola care pipi!
               </CardDescription>
             </CardHeader>
-            
           </Card>
         </TabsContent>
         <TabsContent value="finished">
@@ -38,7 +52,6 @@ export default function Page() {
                 Chupa pija
               </CardDescription>
             </CardHeader>
-            
           </Card>
         </TabsContent>
         <TabsContent value="create">
@@ -48,11 +61,9 @@ export default function Page() {
                 Ana la más linda
               </CardDescription>
             </CardHeader>
-            
           </Card>
         </TabsContent>
       </Tabs>
     </div>
   );
 }
-
