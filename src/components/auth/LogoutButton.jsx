23d95ab04b126f23c07a32be1a24f56800040ce9
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-08-25 09:39:32 -0500

    feat: Add loading state to LogoutButton component

--- src/components/auth/LogoutButton.jsx
+++ src/components/auth/LogoutButton.jsx
@@ -1,17 +1,28 @@
 import { signOut } from "next-auth/react";
 import { Button } from "../ui/button";
+import React from "react";
 
 function handleLogout() {
   signOut({ callbackUrl: "/" });
 }
 
 export default function LogoutButton() {
+  const [loading, setLoading] = React.useState(false);
+
   return (
-    <Button
-      onClick={handleLogout}
-      className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
-    >
-      Cerrar sesión
-    </Button>
+    <>
+      {!loading ? (
+        <Button
+          onClick={handleLogout}
+          className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
+        >
+          Cerrar sesión
+        </Button>
+      ) : (
+        <Button className="px-4 py-2 bg-red-500 text-white rounded" disabled>
+          Cerrando sesión...
+        </Button>
+      )}
+    </>
   );
 }
